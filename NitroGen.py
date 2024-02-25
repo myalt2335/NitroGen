ebcfb6933631a9a4206fdb10a213681686d49f3e
myalt2335 <118089796+myalt2335@users.noreply.github.com>
2024-02-24 19:09:48 -0500

    Update NitroGen.py
    V2.1.5 to Final Version V3.0.0

--- NitroGen.py
+++ NitroGen.py
@@ -5,7 +5,7 @@ import time
 from concurrent.futures import ThreadPoolExecutor
 import re
 
-version = "V2.1.5"
+version = "V3.0.0"
 
 def generate_random_string(length):
     characters = string.ascii_letters + string.digits
@@ -17,27 +17,21 @@ def send_request(code):
         response = requests.get(url)
         response.raise_for_status()
 
-        log(f"Status Code: {response.status_code} - {code}")
-
         if response.status_code == 200:
-            log(f"Valid code found: {code}")
+            print(f"Valid code found: {code}")
             send_to_discord_webhook(code)
         else:
-            log(f"Invalid code: {code}")
+            print(f"Invalid code: {code}")
 
-    except requests.exceptions.RequestException as e:
-        log(f"Request failed for code {code}. Exception: {e}")
+    except requests.exceptions.RequestException:
+        print(f"Request failed for code {code}.")
 
 def send_to_discord_webhook(code):
     payload = {"content": f"New valid code: {code}"}
     try:
         requests.post(discord_webhook_url, json=payload)
     except requests.exceptions.RequestException as e:
-        log(f"Failed to send webhook for code {code}. Exception: {e}")
-
-def log(message):
-    timestamp = time.strftime("[%Y-%m-%d %H:%M:%S]")
-    print(f"{timestamp} {message}")
+        print(f"Failed to send webhook for code {code}. Exception: {e}")
 
 def generate_and_send():
     generated_code = generate_random_string(18)
@@ -47,13 +41,13 @@ def is_valid_webhook(url):
     return re.match(r'https://discord\.com/api/webhooks/\d+/.+', url) is not None
 
 if __name__ == "__main__":
-    log(f"Script started - {version}")
-
     discord_webhook_url = input("Enter your Discord webhook URL: ")
 
     while not is_valid_webhook(discord_webhook_url):
         print("Invalid Discord webhook URL. Please enter a valid URL.")
         discord_webhook_url = input("Enter your Discord webhook URL: ")
+
+    print(f"Script started - {version}")
 
     try:
         with ThreadPoolExecutor(max_workers=10) as executor:
@@ -61,4 +55,4 @@ if __name__ == "__main__":
                 executor.submit(generate_and_send)
                 time.sleep(7)
     except KeyboardInterrupt:
-        log("Script stopped by user")
+        print("Script stopped by user")
